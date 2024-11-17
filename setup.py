f4801e01cc5f3c3a7ca4bc471ac34c9b5d5628e6
Dganna24 <bandzu@icloud.com>
2024-11-17 20:07:39 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='simple-calculator',
+    version='0.1',
+    description='A simple calculator for basic operations.',
+    py_modules=['calculator'],
+    python_requires='>=3.6'
