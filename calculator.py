b78ffb4251088624d60d6a9fbe7bcd31a6eb23d2
Dganna24 <bandzu@icloud.com>
2024-11-17 20:31:04 +0200

    Initial commit

--- calculator.py
+++ calculator.py
@@ -1,17 +1,25 @@
+def power(a, b):
+    return a ** b
+
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Деление на ноль невозможно.")
+    return a / b
+
+
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
 class Calculator:
-    def add(self, a, b):
-        return a + b
+    pass
 
-    def subtract(self, a, b):
-        return a - b
-
-    def multiply(self, a, b):
-        return a * b
-
-    def divide(self, a, b):
-        if b == 0:
-            raise ValueError("Деление на ноль невозможно.")
-        return a / b
-
-    def power(self, a, b):
-        return a ** b
